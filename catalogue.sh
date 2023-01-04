69daf1d7ed7938726b23c8ce0e7ab8c9b7bd33fb
windows 10 <dimpulsunayana205@gmail.com>
2023-01-04 20:23:32 +0530

    updated catalogue conf files

---
+++ catalogue.sh
@@ -0,0 +1,24 @@
+curl -sL https://rpm.nodesource.com/setup_lts.x | bash
+
+#intall nodejs app
+yum install nodejs -y
+
+#create an user
+useradd roboshop
+
+#create a directory called app
+mkdir /app
+curl -L -o /tmp/catalogue.zip https://roboshop-artifacts.s3.amazonaws.com/catalogue.zip
+cd /app
+unzip /tmp/catalogue.zip
+
+npm install
+
+cp ${script_location}/files/catalogue.service /etc/systemd/system/catalogue.service
+
+systemctl daemon-reload
+labauto mongodb-client
+mongo --host localhost </app/schema/catalogue.js
+
+systemctl enable catalogue
+systemctl start catalogue

a6ec685ded945cbf46a10c7154c84a059c214b6c
windows 10 <dimpulsunayana205@gmail.com>
2023-01-08 23:47:02 +0530

    updated catalogue file

--- catalogue.sh
+++ catalogue.sh
@@ -1,30 +1,65 @@
 script_location=$(pwd)
+log=/tmp/roboshop.log
 
-curl -sL https://rpm.nodesource.com/setup_lts.x | bash
+curl -sL https://rpm.nodesource.com/setup_lts.x | bash &>>${log}
+echo -e "\e[35mdownloaded nodejs app\e[0m"
+if [ $? -eq 0 ]
+then echo SUCCESS
+else echo Fail
+fi
 
 #intall nodejs app
-yum install nodejs -y
+yum install nodejs -y &>>${log}
+echo -e "\e[35mnodejs installed\e[0m"
+if [ $? -eq 0 ]
+then echo SUCCESS
+else echo Fail
+fi
 
 #create an user
-useradd roboshop
+useradd roboshop &>>${log}
+echo -e "\e[35muser created to roboshop\e[0m"
+if [ $? -eq 0 ]
+then echo SUCCESS
+else echo Fail
+fi
 
 #create a directory called app
-mkdir /app
-curl -L -o /tmp/catalogue.zip https://roboshop-artifacts.s3.amazonaws.com/catalogue.zip
+mkdir /app &>>${log}
+curl -L -o /tmp/catalogue.zip https://roboshop-artifacts.s3.amazonaws.com/catalogue.zip &>>${log}
+if [ $? -eq 0 ]
+then echo SUCCESS
+else echo Fail
+fi
 cd /app
-unzip /tmp/catalogue.zip
+unzip /tmp/catalogue.zip &>>${log}
+echo -e "\e[35mextracted catalogue file\e[0m"
+if [ $? -eq 0 ]
+then echo SUCCESS
+else echo Fail
+fi
 
-npm install
+npm install &>>${log}
 
-cp ${script_location}/files/catalogue.service /etc/systemd/system/catalogue.service
+cp ${script_location}/files/catalogue.service /etc/systemd/system/catalogue.service &>>${log}
 
-systemctl daemon-reload
+systemctl daemon-reload &>>${log}
 
-cp ${script_location}/files/mongodb.repo /etc/yum.repos.d/mongodb.repo
-yum install mongodb-org-shell -y
+cp ${script_location}/files/mongodb.repo /etc/yum.repos.d/mongodb.repo &>>${log}
+yum install mongodb-org-shell -y &>>${log}
+echo -e "\e[35minstalled mongodb\e[0m"
+if [ $? -eq 0 ]
+then echo SUCCESS
+else echo Fail
+fi
 #labauto mongodb-client
 
-mongo --host mongodb-dev.dimpul.online </app/schema/catalogue.js
+mongo --host mongodb-dev.dimpul.online </app/schema/catalogue.js &>>${log}
 
-systemctl enable catalogue
-systemctl start catalogue
+systemctl enable catalogue &>>${log}
+systemctl start catalogue &>>${log}
+echo -e "\e[35mrestarted catalogue\e[0m"
+if [ $? -eq 0 ]
+then echo SUCCESS
+else echo Fail
+fi

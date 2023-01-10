1997fcc3e066ee460f986f20ade6d9c4f2c725ab
windows 10 <dimpulsunayana205@gmail.com>
2023-01-10 15:51:23 +0530

    updated catalogue file

--- catalogue.sh
+++ catalogue.sh
@@ -1,12 +1,4 @@
-script_location=$(pwd)
-log=/tmp/roboshop.log
-
-status_check(){
-  if [ $? -eq 0 ]
-  then echo -e "\e[32mSUCCESS\e[0m"
-  else echo -e "\e[31mFAILURE\e[0m"
-  fi
-}
+source common.sh
 
 curl -sL https://rpm.nodesource.com/setup_lts.x | bash &>>${log}
 echo -e "\e[35mdownloaded nodejs app\e[0m"
--- frontend.sh
+++ frontend.sh
@@ -1,52 +1,33 @@
-script_location=$(pwd)
-log=/tmp/roboshop.log
+source common.sh
 
 #install nginx server
 yum install nginx -y &>>${log}
 echo -e "\e[35mnginx installed\e[0m"
-if [ $? -eq 0 ]
-then echo SUCCESS
-else echo Fail
-fi
+status_check
 
 #Remove default content in nginx webser server is running
 rm -rf /usr/share/nginx/html/* &>>${log}
 echo -e "\e[35mdefault content is removed\e[0m"
-if [ $? -eq 0 ]
-then echo SUCCESS
-else echo Fail
-fi
+status_check
 
 #download the roboshop-frontend content
 curl -o /tmp/frontend.zip https://roboshop-artifacts.s3.amazonaws.com/frontend.zip &>>${log}
 echo -e "\e[35mdownloaded the fronted content\e[0m"
-if [ $? -eq 0 ]
-then echo SUCCESS
-else echo Fail
-fi
+status_check
 
 #extract the frontend file
 cd /usr/share/nginx/html
 unzip /tmp/frontend.zip &>>${log}
 echo -e "\e[35munzipped the file\e[0m"
-if [ $? -eq 0 ]
-then echo SUCCESS
-else echo Fail
-fi
+status_check
 
 #create reverse proxy conf
 cp ${script_location}/files/frontend_conf.sh /etc/nginx/default.d/roboshop.conf &>>${log}
 echo -e "\e[35mcreated rev proxy\e[0m"
-if [ $? -eq 0 ]
-then echo SUCCESS
-else echo Fail
-fi
+status_check
 
 #enable and start nginx server
 systemctl enable nginx &>>${log}
 systemctl restart nginx &>>${log}
 echo -e "\e[35mnginx restarted\e[0m"
-if [ $? -eq 0 ]
-then echo SUCCESS
-else echo Fail
-fi
+status_check

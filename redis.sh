aa3907712d3376354ac97c14e9ba2851ccb88f0c
windows 10 <dimpulsunayana205@gmail.com>
2023-01-10 18:27:15 +0530

    updated catalogue file

--- redis.sh
+++ redis.sh
@@ -1,16 +1,25 @@
 #Checking the current dir and storing it(like a variable) to use further
-script_location=$(pwd)
+source files/commonfile.sh
 
-#create reverse proxy conf
-cp ${script_location}/files/mongodb.repo /etc/yum.repos.d/mongo.repo
+yum install https://rpms.remirepo.net/enterprise/remi-release-8.rpm -y &>>${log}
+print_head "install redis repo"
+status_check
+
+dnf module enable redis:remi-6.2 -y &>>${log}
+print_head "enable redis 6.2 v"
+status_check
 
 #install mongod server
-yum install mongodb-org -y
+yum install redis -y  &>>${log}
+print_head "installed redis"
+status_check
 
 #sed -stream line editor
 # -e is to verify and -i is proceed to change
 #here its replacing 127.0.0.1 to 0.0.0.0
-sed -i -e 's/127.0.0.1/0.0.0.0/' /etc/mongod.conf
+sed -i -e 's/127.0.0.1/0.0.0.0/' /etc/redis.conf  &>>${log}
 
-systemctl enable mongod
-systemctl restart mongod
+systemctl enable redis  &>>${log}
+systemctl restart redis  &>>${log}
+print_head "redis restarted"
+status_check

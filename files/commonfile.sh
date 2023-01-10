69a9e3044580f74d44dfd9e8706265205bae5e84
windows 10 <dimpulsunayana205@gmail.com>
2023-01-10 16:27:28 +0530

    updated catalogue file

--- files/commonfile.sh
+++ files/commonfile.sh
@@ -3,7 +3,7 @@ log=/tmp/roboshop.log
 
 status_check(){
   if [ $? -eq 0 ]
-  then echo -e "\e[32mSUCCESS\e[0m"
-  else echo -e "\e[31mFAILURE\e[0m"
+  then echo -e "\e[1;32mSUCCESS\e[0m"
+  else echo -e "\e[1;31mFAILURE\e[0m"
   fi
 }

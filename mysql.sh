818cd9d29118f522060ccd6f2505d9fb10a0fdec
windows 10 <dimpulsunayana205@gmail.com>
2023-01-13 22:28:30 +0530

    updated catalogue file

--- mysql.sh
+++ mysql.sh
@@ -1,5 +1,10 @@
 source files/commonfile.sh
 
+if [ -z "${root_mysql_password}" ]; then
+  echo "Variable root_mysql_password is missing"
+  exit 1
+fi
+
 dnf module disable mysql -y &>>${log}
 print_head "module disabled"
 status_check
@@ -13,16 +18,19 @@ print_head "installed mysql"
 status_check
 
 systemctl enable mysqld &>>${log}
-print_head "enabled mysql"
+print_head "enabled mysqld"
 status_check
 
 systemctl start mysqld &>>${log}
-print_head "restarted mysql"
+print_head "restarted mysqld"
 status_check
 
-mysql_secure_installation --set-root-pass RoboShop@1 &>>${log}
-print_head "root pwd got set"
+#mysql_secure_installation --set-root-pass RoboShop@1 &>>${log}
+print_head "Reset Default Database Password"
+mysql_secure_installation --set-root-pass ${root_mysql_password} &>>${log}
 status_check
+#print_head "root pwd got set"
+#status_check
 
 mysql -uroot -pRoboShop@1 &>>${log}
 print_head "user root pwd got set"
